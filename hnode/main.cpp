20f66850363d8f2bb1dc75baa0adfe70d793901f
ubuntu <ubuntu@localhost>
2022-10-27 15:24:38 +0200

    Begin adding support for node 12 (the one in ubuntu 22.04)
    [skip ci]

--- hnode/main.cpp
+++ hnode/main.cpp
@@ -11,6 +11,11 @@ int run(
     const std::vector<std::string>& args,
     const std::vector<std::string>& exec_args)
 {
+    const char main_script_source_utf8[] = {
+#include "main.jsc"
+    };
+
+#if (NODE_MAJOR_VERSION == 19)
     std::vector<std::string> errors;
     auto setup = node::CommonEnvironmentSetup::Create(
         platform, &errors, args, exec_args);
@@ -31,10 +36,6 @@ int run(
         v8::HandleScope handle_scope(isolate);
         v8::Context::Scope context_scope(setup->context());
 
-        const char main_script_source_utf8[] = {
-#include "main.jsc"
-        };
-
         auto loadenv_ret = node::LoadEnvironment(env, main_script_source_utf8);
         if (loadenv_ret.IsEmpty()) {
             return 1;
@@ -46,6 +47,77 @@ int run(
     }
 
     return exit_code;
+#elif (NODE_MAJOR_VERSION == 12)
+
+    uv_loop_t loop;
+    int r = uv_loop_init(&loop);
+    assert(r != 0);
+
+    std::shared_ptr<node::ArrayBufferAllocator> allocator =
+        node::ArrayBufferAllocator::Create();
+
+    v8::Isolate* isolate = node::NewIsolate(allocator.get(), &loop, platform);
+
+    int exit_code = 0;
+    {
+        v8::Locker locker(isolate);
+        v8::Isolate::Scope isolate_scope(isolate);
+        std::unique_ptr<node::IsolateData, decltype(&node::FreeIsolateData)> isolate_data(
+            node::CreateIsolateData(isolate, &loop, platform, allocator.get()),
+            node::FreeIsolateData);
+
+        v8::HandleScope handle_scope(isolate);
+        v8::Local<v8::Context> context = node::NewContext(isolate);
+        v8::Context::Scope context_scope(context);
+
+        std::unique_ptr<node::Environment, decltype(&node::FreeEnvironment)> env(
+              node::CreateEnvironment(isolate_data.get(), context, args, exec_args),
+              node::FreeEnvironment);
+
+        auto loadenv_ret = node::LoadEnvironment(env.get(), main_script_source_utf8);
+        if (loadenv_ret.IsEmpty()) {
+            return 1;
+        }
+
+        {
+            v8::SealHandleScope seal(isolate);
+            bool more;
+            do {
+                uv_run(&loop, UV_RUN_DEFAULT);
+
+                platform->DrainTasks(isolate);
+
+                more = uv_loop_alive(&loop);
+                if(more) continue;
+
+                node::EmitBeforeExit(env.get());
+
+                more = uv_loop_alive(&loop);
+            } while(more == true);
+        }
+
+        exit_code = node::EmitExit(env.get());
+
+        node::Stop(env.get());
+    }
+
+    bool platform_finished = false;
+    platform->AddIsolateFinishedCallback(isolate, [](void* data) {
+        *static_cast<bool*>(data) = true;
+    }, &platform_finished);
+    platform->UnregisterIsolate(isolate);
+    isolate->Dispose();
+
+    while(!platform_finished) {
+        uv_run(&loop, UV_RUN_ONCE);
+    }
+
+    uv_loop_close(&loop);
+
+    return exit_code;
+#else
+#error "unsupported node version"
+#endif
 }
 
 int main(int argc, char* argv[])
@@ -55,29 +127,55 @@ int main(int argc, char* argv[])
 
     argv = uv_setup_args(argc, argv);
     std::vector<std::string> args(argv, argv + argc);
+
+#if (NODE_MAJOR_VERSION == 19)
     auto result = node::InitializeOncePerProcess(
         args, {
             node::ProcessInitializationFlags::kNoInitializeV8,
             node::ProcessInitializationFlags::kNoInitializeNodeV8Platform
         });
-
     for (const std::string& err: result->errors()) {
         error("%s", err.c_str());
     }
     if (result->early_return() != 0) {
         return result->exit_code();
     }
+#elif (NODE_MAJOR_VERSION == 12)
+    std::vector<std::string> exec_args;
+    std::vector<std::string> errors;
+    int exit_code = node::InitializeNodeWithArgs(&args, &exec_args, &errors);
+    for (const std::string& err: errors) {
+        error("%s", err.c_str());
+    }
+    if(exit_code != 0) {
+        return exit_code;
+    }
+#else
+#error "unsupported node version"
+#endif
 
     auto platform = node::MultiIsolatePlatform::Create(1);
     v8::V8::InitializePlatform(platform.get());
     v8::V8::Initialize();
 
+#if (NODE_MAJOR_VERSION == 19)
     int r = run(platform.get(), result->args(), result->exec_args());
+#elif (NODE_MAJOR_VERSION == 12)
+    int r = run(platform.get(), args, exec_args);
+#else
+#error "unsupported node version"
+#endif
 
     v8::V8::Dispose();
-    v8::V8::DisposePlatform();
 
+#if (NODE_MAJOR_VERSION == 19)
+    v8::V8::DisposePlatform();
     node::TearDownOncePerProcess();
+#elif (NODE_MAJOR_VERSION == 12)
+    v8::V8::ShutdownPlatform();
+#else
+#error "unsupported node version"
+#endif
 
     return r;
 }

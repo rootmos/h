0ee29809f0301e325e8751be7f9f60d130afa4d8
Gustav Behm <me@rootmos.io>
2022-10-27 11:50:05 +0200

    Use the dedicated seccomp syscall instead of prctl

--- build/seccomp.c
+++ build/seccomp.c
@@ -1,6 +1,13 @@
 #include <linux/seccomp.h>
 #include <linux/filter.h>
-#include <sys/prctl.h>
+#include <sys/syscall.h>
+
+#ifndef seccomp
+static int seccomp(unsigned int operation, unsigned int flags, void *args)
+{
+    return syscall(SYS_seccomp, operation, flags, args);
+}
+#endif
 
 void seccomp_apply_filter()
 {
@@ -9,6 +16,6 @@ void seccomp_apply_filter()
     };
 
     struct sock_fprog p = { .len = LENGTH(filter), .filter = filter };
-    int r = prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &p);
-    CHECK(r, "prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER)");
+    int r = seccomp(SECCOMP_SET_MODE_FILTER, 0, &p);
+    CHECK(r, "seccomp(SECCOMP_SET_MODE_FILTER)");
 }

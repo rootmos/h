a582a58f855220754fc7ae90cee03d7dd08fc460
Gustav Behm <me@rootmos.io>
2022-10-30 08:27:40 +0100

    Parse options in hnode

--- hnode/main.cpp
+++ hnode/main.cpp
@@ -7,15 +7,68 @@
 #include "capabilities.c"
 #include "seccomp.c"
 
+struct options {
+    const char* input;
+};
+
+static void print_usage(int fd, const char* prog)
+{
+    dprintf(fd, "usage: %s [OPTION]... INPUT\n", prog);
+    dprintf(fd, "\n");
+    dprintf(fd, "options:\n");
+    dprintf(fd, "  -h       print this message\n");
+    dprintf(fd, "  -v       print version information\n");
+}
+
+#include "version.c"
+
+static void parse_options(struct options* o, int argc, char* argv[])
+{
+    memset(o, 0, sizeof(*o));
+
+    int res;
+    while((res = getopt(argc, argv, "hv")) != -1) {
+        switch(res) {
+        case 'v':
+            print_version(argv[0]);
+            exit(0);
+        case 'h':
+        default:
+            print_usage(res == 'h' ? 1 : 2, argv[0]);
+            exit(res == 'h' ? 0 : 1);
+        }
+    }
+
+    if(optind < argc) {
+        o->input = argv[optind];
+        debug("input: %s", o->input);
+
+        struct stat st;
+        int r = stat(o->input, &st);
+        if(r == -1 && errno == ENOENT) {
+            dprintf(2, "error; unable to access input file: %s\n", o->input);
+            exit(1);
+        }
+        CHECK(r, "stat(%s)", o->input);
+    } else {
+        dprintf(2, "error: no input file specified\n");
+        print_usage(2, argv[0]);
+        exit(1);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     drop_capabilities();
     no_new_privs();
 
+    struct options o;
+    parse_options(&o, argc, argv);
+
     int rsfd = landlock_new_ruleset();
 
-    debug("allowing read access: %s", argv[1]);
-    landlock_allow_read(rsfd, argv[1]);
+    debug("allowing read access: %s", o.input);
+    landlock_allow_read(rsfd, o.input);
 
     landlock_apply(rsfd);
     int r = close(rsfd); CHECK(r, "close");

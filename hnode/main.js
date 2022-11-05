3ca3fa797a92e1806f7dfd2eea3e65609a2fbc6d
Gustav Behm <me@rootmos.io>
2022-11-05 12:36:11 +0100

    Add tests for exec rejection

--- hnode/main.js
+++ hnode/main.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const vm = require('vm');
 
+globalThis.require = require('module').createRequire(process.cwd() + '/');
+
 fs.readFile(input_script_filename, 'utf8', function(err, data) {
     new vm.Script(data, { filename: input_script_filename }).runInThisContext();
 });

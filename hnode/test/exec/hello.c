abd529a67f7edc5cd82fcb2d1e66df97a536109d
Gustav Behm <me@rootmos.io>
2025-05-12 18:16:00 +0200

    Prepare the test in the test-runner

--- hnode/test/exec/hello.c
+++ hnode/test/exec/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    dprintf(2, "hello\n");
+    printf("hello\n");
     return 0;
 }
--- hnode/test/exec/main.js
+++ hnode/test/exec/main.js
@@ -1,2 +1,1 @@
-//require('child_process').execSync('echo "hello" 1>&2')
 require('child_process').spawnSync('./hello', [], { env: [], stdio: 'inherit' })
